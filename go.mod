397fa6c87e81947c0e76e5e9f1750fce4a3fca70
fantarqse <fantarose51@gmail.com>
2022-04-11 11:48:05 +0300

    v 0.5

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module registration-server
 go 1.17
 
 require github.com/lib/pq v1.10.4
+
+require golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29 // indirect
--- main.go
+++ main.go
@@ -3,18 +3,50 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
-	_ "github.com/lib/pq"
 	"log"
 	"net/http"
+	"regexp"
+	"strings"
+
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	emailReg    string = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+	passwordReg string = "[A-Z]+[a-z]+[^a-zA-Z]{2,}"
 )
 
 var db *sql.DB
 
 type Users struct {
-	Login    string `json:"login,omitempty", db: "login"`
-	Password string `json:"password,omitempty", db: "password"`
-	Email    string `json:"email,omitempty", db: "email"`
+	Login      *string `json:"login,omitempty" db:"login"`
+	Password   *string `json:"password,omitempty" db:"password"`
+	Email      *string `json:"email,omitempty" db:"email"`
+	IsVerified bool    `json:"isVerified,omitempty" db:"is_verified"`
+}
+
+func (u Users) validator() error {
+	email := regexp.MustCompile(emailReg)
+	password := regexp.MustCompile(passwordReg)
+
+	if len(*u.Login) < 3 {
+		log.Println("Login is not valid!")
+		return errors.New("error: login is too short")
+	}
+
+	if len(*u.Password) < 8 || !password.MatchString(*u.Password) {
+		log.Println("Password is not valid!")
+		return errors.New("error: password is not valid")
+	}
+
+	if !email.MatchString(*u.Email) {
+		log.Println("Email is not valid!")
+		return errors.New("error: email is not valid")
+	}
+	return nil
 }
 
 func initDB() {
@@ -28,21 +60,44 @@ func initDB() {
 }
 
 func signUpHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "Sign Up")
-
 	users := &Users{}
 	err := json.NewDecoder(req.Body).Decode(users)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		http.Error(w, "Bad request: JSON is not valid", 400)
 		return
 	}
 
-	if _, err = db.Query("insert into users values ($1, $2, $3)", users.Login, users.Password, users.Email); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Println(http.StatusInternalServerError)
+	validationErr := users.validator()
+	if validationErr != nil {
+		http.Error(w, validationErr.Error(), 400)
 		return
 	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(*users.Password), 8)
+
+	if _, err = db.Query(
+		"insert into users (login, password, email) values ($1, $2, $3)",
+		users.Login,
+		string(hashedPassword),
+		users.Email,
+	); err != nil {
+		log.Println(err.Error())
+
+		if strings.Contains(err.Error(), "повторювані значення ключа") {
+			http.Error(w, "Username or Email is not unique.", 500)
+			return
+
+		} else if strings.Contains(err.Error(), "порушує not-null") {
+			http.Error(w, "Username, Password and Email are required.", 500)
+			return
+
+		} else {
+			http.Error(w, err.Error(), 500)
+			return
+		}
+	}
+
+	fmt.Fprintf(w, "Sign Up")
 }
 
 func signInHandler(w http.ResponseWriter, req *http.Request) {
